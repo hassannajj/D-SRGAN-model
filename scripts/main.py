43ad398896baea5adb54db8cb450b28ea1389ae8
hassannajj <hhalnajj@uci.edu>
2024-09-29 18:06:14 -0700

    fixed main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,14 +1,18 @@
+import os
 import subprocess
 
-command = [
-    'python3',
-    '../training.py', 
-    '../training_testing_data/train/LR', 
-    '../training_testing_data/train/HR', 
-    '../training_testing_data/test/LR', 
-    '../training_testing_data/HR'
-]
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.realpath(__file__))
 
+# Build paths relative to the script directory
+command = [
+    'python3', 
+    os.path.join(script_dir, '../training.py'),
+    os.path.join(script_dir, '../training_testing_data/train/LR'),
+    os.path.join(script_dir, '../training_testing_data/train/HR'),
+    os.path.join(script_dir, '../training_testing_data/test/LR'),
+    os.path.join(script_dir, '../training_testing_data/HR')
+]
 # Run the command
 result = subprocess.run(command, capture_output=False, text=True)
 
